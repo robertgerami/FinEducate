78f8c55004a9e7b77e0b3735d76d9ae5f387c0dd
robertgerami <31670940+robertgerami@users.noreply.github.com>
2018-07-03 16:21:02 -0400

    Prototype
    this is just a sample of what it could possibly possibly look like

---
+++ FinEducate/ViewController2.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController2.swift
+//  FinEducate
+//
+//  Created by Bobby Gerami on 7/3/18.
+//  Copyright © 2018 Bobby Gerami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sideMenus()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sideMenus(){
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 200
+            revealViewController().rightViewRevealWidth = 160
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
